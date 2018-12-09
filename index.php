cfd479d7c219179eb331886dd4b824a15b44baad
Evgeny <paltseveugeny@gmail.com>
2018-12-09 19:56:23 +0300

    Первый коммит в Git

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Test</title>
+</head>
+
+
+<body>
+
+<div id="wrap">
+    <h1>Заголовок</h1>
+</div>
+
+<?php
+
+
+echo 'Hello';
+
+
+?>
+
+<script src="js/js.js"></script>
+</body>
+</html>
